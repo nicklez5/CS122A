6e94282cf9b0fe4f56814c3f370e523e82990aab
nicklez5 <jlu080@ucr.edu>
2019-10-02 18:09:25 -0700

    Initializing repository with lab files

---
+++ Lab1_AVRToolchainReview/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab1_AVRToolchainReview.c
+ *
+ * Created: 10/2/2019 6:06:44 PM
+ * Author : jackson lu
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
